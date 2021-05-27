27fb09d125ecae774d0039e2d0fa96cd55767720
rainierrr <b1916320@planet.kanazawa-it.ac.jp>
2021-05-27 23:34:09 +0900

    1.two-sum

--- 1.two-sum.py
+++ 1.two-sum.py
@@ -6,7 +6,13 @@
 
 # @lc code=start
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def twoSum(self, num, target):
+        d = dict()
+        for index, number in enumerate(num):
+            try:
+                return ((d[target-number]+1, index+1))
+            except:
+                d[number] = index
 
 # @lc code=end
 
